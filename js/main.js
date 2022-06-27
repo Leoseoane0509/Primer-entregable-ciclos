d19a6dfe2ef0a9cee523300d50d8cbe82fb63346
Leonel Emanuel Seoane <leoemanuel2009.ls@gmail.com>
2022-06-26 22:48:23 -0300

    dos ejemplos con whil

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-// Ciclo con FOR
+// Ciclos con FOR
 console.log("*******************FOR********************");
 // Ejemplo 1
-console.log("******************Ejemplo 1***********");
+console.log("**********Ejemplo 1**********");
 let miArreglo = [4, 6, 8, 2];
 let total = 0;
 for (let i = 0; i < miArreglo.length; i++) {
@@ -13,9 +13,28 @@ console.log(
   "El resultado de la suma de cada elemento del arreglo es: " + total
 );
 // Ejemplo 2
-console.log("**************Ejemplo 2**************");
+console.log("**********Ejemplo 2**********");
 let lenguajes = ["JavaScript", "Python", "Java", "C++"];
 for (let i = 0; i < lenguajes.length; i++) {
   console.log(lenguajes[i].toUpperCase());
 }
 /****************************************************************** */
+// Ciclos con while
+console.log("********************WHILE***************");
+// Ejemplo 1
+console.log("**********Ejemplo 1**********");
+let miArreglo2 = [];
+let i = 0;
+console.log(miArreglo2);
+while (i < 10) {
+  miArreglo2.push(i);
+  i++;
+}
+console.log(miArreglo2);
+// Ejemplo 2
+console.log("**********Ejemplo 2**********");
+let numeros = [2, 3, 4, 5, 6, 8, 9, 34];
+while (numeros.length > 4) {
+  numeros.pop();
+}
+console.log(numeros);
